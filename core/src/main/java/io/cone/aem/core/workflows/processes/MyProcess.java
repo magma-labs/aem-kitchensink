7784feb7e9b54de08a4b425d1860cb2aedbb84c5
Cone <carlos.gutierrez@magmalabs.io>
2018-03-06 12:27:34 -0600

    Adds example of a workflow process step

---
+++ core/src/main/java/io/cone/aem/core/workflows/processes/MyProcess.java
@@ -0,0 +1,46 @@
+package io.cone.aem.core.workflows.processes;
+import com.day.cq.workflow.WorkflowException;
+import com.day.cq.workflow.WorkflowSession;
+import com.day.cq.workflow.exec.WorkItem;
+import com.day.cq.workflow.exec.WorkflowData;
+import com.day.cq.workflow.exec.WorkflowProcess;
+import com.day.cq.workflow.metadata.MetaDataMap;
+import org.osgi.framework.Constants;
+import org.osgi.service.component.annotations.Component;
+
+import javax.jcr.Node;
+import javax.jcr.RepositoryException;
+
+@Component(
+        service=WorkflowProcess.class,
+        property={
+                Constants.SERVICE_DESCRIPTION + "=A sample workflow process implementation.",
+                Constants.SERVICE_VENDOR + "=Adobe",
+                "process.label=My Sample Workflow Process"
+        }
+)
+public class MyProcess implements WorkflowProcess {
+
+  private static final String TYPE_JCR_PATH = "JCR_PATH";
+
+  public void execute(WorkItem item, WorkflowSession session, MetaDataMap args) throws WorkflowException {
+    WorkflowData workflowData = item.getWorkflowData();
+    if (workflowData.getPayloadType().equals(TYPE_JCR_PATH)) {
+      String path = workflowData.getPayload().toString() + "/jcr:content";
+      try {
+        Node node = (Node) session.getSession().getItem(path);
+        if (node != null) {
+          node.setProperty("approved", readArgument(args));
+          session.getSession().save();
+        }
+      } catch (RepositoryException e) {
+        throw new WorkflowException(e.getMessage(), e);
+      }
+    }
+  }
+
+  private boolean readArgument(MetaDataMap args) {
+    String argument = args.get("PROCESS_ARGS", "false");
+    return argument.equalsIgnoreCase("true");
+  }
+}
